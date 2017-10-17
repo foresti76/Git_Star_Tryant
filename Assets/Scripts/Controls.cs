accf23f62e933b79706ef46f27891481a3bff09e
Forest Ingram <forestrmi@gmail.com>
2017-10-17 09:27:52 -0700

    Adding stub for ship customization and controls

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+    //Todo Move all keyboard and other controls into here
+	// Use this for initialization
+    
+        //Make references for all the scrips that have controls in them
+        //mainly ship movement/shields/inventory/shipcustomization
+
+
+
+	void Start () {
+		//find the objects with those scripts
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//call scripts based on keyboard input
+	}
+}
