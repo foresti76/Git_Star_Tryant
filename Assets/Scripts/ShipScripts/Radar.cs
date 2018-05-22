bee3cd652334597921d0accfbd17a177eab56a5a
Demo Laptop <forestrmi@hotmail.com>
2018-05-21 17:11:24 -0700

    Radar detection now works
    A successfull detection will show the icon and add the gameObject to the detections list.  Moving outside of radar range will remove the detection and hide the icon, except for the player.  Had to add new layers for detectors and detections to keep from running into every collider.

--- Assets/Scripts/ShipScripts/Radar.cs
+++ Assets/Scripts/ShipScripts/Radar.cs
@@ -10,16 +10,21 @@ public class Radar : MonoBehaviour {
     public int targetingSpeed;
     public int energyCost;
     public int signature;
+    public SphereCollider radarTrigger;
+    public GameObject miniMapIcon;
+    public List<GameObject> detections;
 
 
     Camera minimapCamera;
 	// Use this for initialization
 	void Start () {
         minimapCamera = GameObject.Find("MiniMapCamera").GetComponent<Camera>();
+        radarTrigger = GetComponentInChildren<SphereCollider>();
 	}
 	
     public void UpdateMinimap()
     {
         minimapCamera.orthographicSize = range;
+        radarTrigger.radius = range;
     }
 }
---
+++ Assets/Scripts/ShipScripts/RadarDetection.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarDetection : MonoBehaviour {
+    public Radar myRadar;
+
+	// Use this for initialization
+	void Start () {
+        myRadar = transform.GetComponentInParent<Radar>();
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        ECM otherECM = other.gameObject.GetComponent<ECM>();
+        if (otherECM && Vector3.Distance(transform.position, other.transform.position) <= myRadar.range - otherECM.detectionDefense && other.tag != "Level")
+        {
+            Radar otherRadar = other.GetComponent<Radar>();
+            otherRadar.miniMapIcon.SetActive(true);
+            myRadar.detections.Add(other.gameObject);
+        }      
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Radar otherRadar = other.gameObject.GetComponent<Radar>();
+        if (otherRadar && other.tag != "Player")
+        {
+            otherRadar.miniMapIcon.SetActive(false);
+        }
+        myRadar.detections.Remove(other.gameObject);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        //todo remove things from my list and hide thier minimap icon if they are out of range.
+        if (myRadar.detections.Contains(other.gameObject))
+        {
+            return;
+        }
+        ECM otherECM = other.GetComponent<ECM>();
+        if (otherECM && Vector3.Distance(transform.position, other.transform.position) <= myRadar.range - otherECM.detectionDefense && other.tag != "Level")
+        {
+            Radar otherRadar = other.GetComponent<Radar>();
+            otherRadar.miniMapIcon.SetActive(true);
+            myRadar.detections.Add(other.gameObject);
+        }
+    }
+
+}
