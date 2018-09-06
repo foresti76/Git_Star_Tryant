419235952c54e91adf9762963d4297eca6d8aa7f
Forest Ingram <forestrmi@gmail.com>
2018-09-06 08:05:21 -0700

    Got the data structure in place for the loot system

---
+++ Assets/Scripts/LootTable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootTable : MonoBehaviour {
+
+    [System.Serializable]
+    public class Loot
+    {
+        public int LootObjectID;
+        public string Rarity;
+
+        public Loot()
+        {
+        }
+    }
+
+
+    public Loot[] lootTable_0;
+    public Loot[] lootTable_1;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
