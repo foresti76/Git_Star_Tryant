fdba06462fc189dc1dba9c5e41c4fd55b94b8fd5
Forest Ingram <forestrmi@gmail.com>
2019-06-03 09:09:34 -0700

    More work on arena mode
    Got some buttons and some ship data to have the player set up arena mode.  Now just need to hook them together.

--- Assets/Scripts/StarBase.cs
+++ Assets/Scripts/StarBase.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class StarBase : MonoBehaviour {
 
-
+    public string starBaseName;
+    public ShipSpawner shipSpawner;
+    public List<ShipData> arenaShips = new List<ShipData>();
     //todo add in station inventory.
     //todo add in station systems
 
     // Use this for initialization
     void Start () {
-		
-	}
+        starBaseName = "Deep Space Ten";
+        arenaShips.Add(shipSpawner.FetchShipDataByID(3));
+        arenaShips.Add(shipSpawner.FetchShipDataByID(4));
+        arenaShips.Add(shipSpawner.FetchShipDataByID(5));
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
 }
